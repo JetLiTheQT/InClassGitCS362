a7c6f8db503e8a5d3b7282f4312b0e681176b12a
Ekkachai Jet Ittihrit <jet@ekkachais-mbp.lan>
2021-04-15 16:50:11 -0700

    ALl 4

--- calc.py
+++ calc.py
@@ -1,14 +1,7 @@
 def calc(a, b):
     sum = a+b
-    print(sum)
-
     difference = a-b
-    print(difference)
-    
     multiply = a*b
-    print(multiply)
-
     quotient = a/b
-    print(quotient)
 
 
